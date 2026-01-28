05e256e4e3f140c72ffcbf8dc4b646c9b4f7b8f4
Manasha <23suca42@tcarts.in>
2026-01-28 19:11:26 +0530

    Add .gitignore and clean build artifacts

--- src/main/java/com/booknest/bookreview/controller/BookReviewController.java
+++ src/main/java/com/booknest/bookreview/controller/BookReviewController.java
@@ -12,7 +12,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(
+        origins = "http://localhost:3002",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+)
+
 public class BookReviewController {
 
     private final BookReviewService service;
