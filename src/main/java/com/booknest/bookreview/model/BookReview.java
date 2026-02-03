ff66ea9fa93a56e2bdfb4983c1ffbab52df1cdd0
MANASHA M <23suca42@tcarts.in>
2026-02-03 10:45:57 +0530

    Remove AllArgsConstructor from BookReview
    Removed the AllArgsConstructor annotation from BookReview class.

--- src/main/java/com/booknest/bookreview/model/BookReview.java
+++ src/main/java/com/booknest/bookreview/model/BookReview.java
@@ -3,13 +3,11 @@ package com.booknest.bookreview.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "book_reviews")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class BookReview {
 
     @Id
