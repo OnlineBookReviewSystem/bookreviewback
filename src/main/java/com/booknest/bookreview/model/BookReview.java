1dd1da7137a2f6be6bf2c5942786c45def6e42bc
MANASHA M <23suca42@tcarts.in>
2026-02-03 10:36:17 +0530

    Update BookReview.java

--- src/main/java/com/booknest/bookreview/model/BookReview.java
+++ src/main/java/com/booknest/bookreview/model/BookReview.java
@@ -3,42 +3,14 @@ package com.booknest.bookreview.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "book_reviews")
 @Data
 @NoArgsConstructor
-public class BookReview {package com.booknest.bookreview.model;
-
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "book_reviews")
-@Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class BookReview {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private String bookTitle;
-
-    @Column(nullable = false)
-    private String author;
-
-    @Column(nullable = false)
-    private String genre;
-
-    @Column(nullable = false)
-    private int rating;
-
-    @Column(length = 1000, nullable = false)
-    private String reviewText;
-}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
