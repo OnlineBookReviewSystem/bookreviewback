fec69641beca67604f9bde054a7b4a587ec9a35f
MANASHA M <23suca42@tcarts.in>
2026-02-03 10:48:48 +0530

    Refactor BookReview class to update rating type
    Changed rating type from int to String and added constructors and getters/setters.

--- src/main/java/com/booknest/bookreview/model/BookReview.java
+++ src/main/java/com/booknest/bookreview/model/BookReview.java
@@ -1,13 +1,9 @@
 package com.booknest.bookreview.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "book_reviews")
-@Data
-@NoArgsConstructor
 public class BookReview {
 
     @Id
@@ -24,8 +20,66 @@ public class BookReview {
     private String genre;
 
     @Column(nullable = false)
-    private int rating;
+    private String rating;   // ✅ FIXED
 
     @Column(length = 1000, nullable = false)
     private String reviewText;
+
+    public BookReview() {}
+
+    public BookReview(String bookTitle, String author, String genre, String rating, String reviewText) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.genre = genre;
+        this.rating = rating;
+        this.reviewText = reviewText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
 }
