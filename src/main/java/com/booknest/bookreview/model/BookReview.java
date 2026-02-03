e731e9c7d8b888fc9358703b66e52f0bc40126b6
MANASHA M <23suca42@tcarts.in>
2026-02-03 10:33:39 +0530

    Refactor BookReview class structure and fields

--- src/main/java/com/booknest/bookreview/model/BookReview.java
+++ src/main/java/com/booknest/bookreview/model/BookReview.java
@@ -8,7 +8,37 @@ import lombok.NoArgsConstructor;
 @Table(name = "book_reviews")
 @Data
 @NoArgsConstructor
+public class BookReview {package com.booknest.bookreview.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book_reviews")
+@Data
+@NoArgsConstructor
 public class BookReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String bookTitle;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(length = 1000, nullable = false)
+    private String reviewText;
+}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
