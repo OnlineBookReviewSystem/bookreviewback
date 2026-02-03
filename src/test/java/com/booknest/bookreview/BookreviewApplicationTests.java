780140208d7535c22bf627997151c141c57f33e8
MANASHA M <23suca42@tcarts.in>
2026-02-03 10:20:59 +0530

    Remove unnecessary blank lines in test class

--- src/test/java/com/booknest/bookreview/BookreviewApplicationTests.java
+++ src/test/java/com/booknest/bookreview/BookreviewApplicationTests.java
@@ -1,14 +1,10 @@
 package com.booknest.bookreview;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class BookreviewApplicationTests {
-
     @Test
     void contextLoads() {
         //This tests ensures that the spring application context loads successfully
     }
-
 }
